6e731e53ca5869ac0b91330144e34357cd53fba3
imwoo90 <wim90@kakao.com>
2025-08-10 16:00:12 +0900

    daisyUI template

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
 use dioxus::prelude::*;
-use reqwest::*;
-
-#[derive(serde::Deserialize)]
-struct DogApi {
-    message: String,
-}
-
-static CSS: Asset = asset!("assets/main.css");
 
 fn main() {
     dioxus::launch(App);
@@ -15,49 +7,64 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Stylesheet { href: CSS }
-        Title {}
-        DogView {}
-    }
-}
-
-#[component]
-fn Title() -> Element {
-    rsx! {
-        div { id: "title",
-            h1 { "HotDog! 🌭" }
-        }
-    }
-}
-
-#[component]
-fn DogView() -> Element {
-    let mut img_src = use_resource(|| async move {
-        reqwest::get("https://dog.ceo/api/breeds/image/random")
-            .await
-            .unwrap()
-            .json::<DogApi>()
-            .await
-            .unwrap()
-            .message
-    });
-
-    let skip = move |evt| { };
-
-    rsx! {
-        div { id: "dogview",
-            img { src: img_src.cloned().unwrap_or_default() }
-        }
-        div { id: "buttons",
-            button {
-                id: "skip",
-                onclick: skip, 
-                "skip",
+        document::Stylesheet { href: asset!("assets/tailwind.css") },
+        main {
+            "data-theme": "cupcake",
+            class: "h-screen flex flex-col",
+            div {
+                class: "navbar bg-base-100",
+                div {
+                    class: "flex-1",
+                    a {
+                        class: "btn btn-ghost text-xl",
+                        href: "#",
+                        "daisyUI"
+                    }
+                }
+                div {
+                    class: "flex-none",
+                    ul {
+                        class: "menu menu-horizontal px-1",
+                        li { a { href: "#", "Link" } }
+                        li {
+                            details {
+                                summary { "Parent" }
+                                ul {
+                                    class: "p-2 bg-base-100 rounded-t-none",
+                                    li { a { href: "#", "Link 1" } }
+                                    li { a { href: "#", "Link 2" } }
+                                }
+                            }
+                        }
+                    }
+                }
             }
-            button {
-                id: "save",
-                onclick: move |_| img_src.restart(),
-                "save!" 
+            div {
+                class: "hero flex-grow bg-base-200",
+                div {
+                    class: "hero-content text-center",
+                    div {
+                        class: "max-w-md",
+                        h1 {
+                            class: "text-5xl font-bold",
+                            "Hello there"
+                        }
+                        p {
+                            class: "py-6",
+                            "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi."
+                        }
+                        button {
+                            class: "btn btn-primary",
+                            "Get Started"
+                        }
+                    }
+                }
+            }
+            footer {
+                class: "footer footer-center p-4 bg-base-300 text-base-content",
+                aside {
+                    p { "Copyright © 2024 - All right reserved by ACME Industries Ltd" }
+                }
             }
         }
     }
